fda253dec240a8c68882f224143187f4640777ae
ayushman_tamrakar <ayushmantamrakar1nitb@gmail.com>
2025-06-08 18:46:07 +0530

    logo name on auth page

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors");  
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
